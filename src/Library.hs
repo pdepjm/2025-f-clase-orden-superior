b55f718a13797d371180344307dec05aa7eb26c0
Tomas Floxo <tomas.floxo@itti.digital>
2025-05-08 13:24:14 -0300

    resolucion en clase

--- src/Library.hs
+++ src/Library.hs
@@ -8,9 +8,9 @@ data Jedi = UnJedi{
     midichlorians :: Number,
     maestros :: [Jedi],
     rango :: Rango
-    } deriving Show
+    }  deriving Show
 
-data Rango = Aprendiz | Padawan | Knight | Master | GrandMaster deriving Show
+data Rango = Aprendiz | Padawan | Knight | Master | GrandMaster deriving (Show, Ord, Eq)
 
 
 anakin :: Jedi
@@ -26,8 +26,62 @@ quiGon :: Jedi
 quiGon = UnJedi{rango = Master, midichlorians = 8000, maestros = [yoda, yaddle]}
 
 yoda :: Jedi
-yoda = UnJedi{rango = GrandMaster, midichlorians = 10000, maestros = []}
+yoda = UnJedi{rango = GrandMaster, midichlorians = 100000, maestros = []}
 
 yaddle :: Jedi
 yaddle = UnJedi{rango = Master, midichlorians = 10000, maestros = []}
 
+
+potencialDeCombate :: Jedi -> Number
+potencialDeCombate jedi = factorDeExperiencia (rango jedi) * (0.05 * midichlorians jedi)
+
+factorDeExperiencia :: Rango -> Number
+factorDeExperiencia Aprendiz = 0
+factorDeExperiencia Padawan = 0.5
+factorDeExperiencia Knight = 1
+factorDeExperiencia Master = 2
+factorDeExperiencia GrandMaster = 2.5
+
+estaPreparado :: Jedi -> Bool
+estaPreparado jedi = potencialDeCombate jedi > 5000 && tieneExperiencia jedi
+
+tieneExperiencia :: Jedi -> Bool
+tieneExperiencia jedi = rango jedi > Padawan
+
+meditar :: Jedi -> Jedi
+meditar jedi = jedi{midichlorians = midichlorians jedi * 1.10}
+
+type Batallon = [Jedi]
+
+caminanteEstrellas :: Batallon
+caminanteEstrellas = [anakin, obiwan, quiGon]
+
+primeroEnFila :: Batallon -> Jedi
+primeroEnFila batallon = head batallon
+
+terceroEnFila :: Batallon -> Jedi
+terceroEnFila batallon = batallon !! 2
+
+combinar :: Batallon -> Batallon -> Batallon
+combinar batallon1 batallon2 = batallon1 ++ batallon2
+
+sumar :: Batallon -> Jedi -> Batallon
+sumar batallon jedi = jedi : batallon
+
+sumar' :: Batallon -> Jedi -> Batallon
+sumar' batallon jedi = [jedi] ++ batallon
+
+estaListo :: Batallon -> Bool
+estaListo batallon = all estaPreparado batallon
+
+seLaBanca :: Batallon -> Bool
+seLaBanca batallon = any tieneExperiencia batallon
+
+losExperimentados :: Batallon -> [Jedi]
+losExperimentados batallon = filter tieneExperiencia batallon
+
+meditacionGrupal :: Batallon -> Batallon
+meditacionGrupal batallon = map meditar batallon
+
+potencialTotal :: Batallon -> Number
+potencialTotal batallon = sum(map potencialDeCombate batallon)
--- src/Spec.hs
+++ src/Spec.hs
@@ -8,4 +8,9 @@ correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
+    it "Un jedi esta preparado si su potencial de combate es mayor a 5000 y tiene experiencia" $ do
+      estaPreparado yoda `shouldBe` True
+    it "Un jedi no esta preparado si no tiene experiencia" $ do
+      estaPreparado anakin `shouldBe` False
+
 
